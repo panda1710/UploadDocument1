6d83f685125a57c0ed87a35033e1496d0b5ea7ce
unknown <lavichoudhary0222@gmail.com>
2021-07-03 23:01:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
+import Modal from "react-modal";
+import React, { useState } from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [modalOpen, setModalOpen] = useState(true);
+  if (modalOpen) {
+    return (
+      // widget
+      <div className="App">
+        <Modal isOpen={modalOpen}>
+          <h1>Widget</h1>
+          <h2>Please Upload your Documents</h2>
+          <p>Click the button to upload </p>
+          {/* form */}
+          <form>
+            <input type="file" name="picture"></input>
+            <button onClick={() => setModalOpen(false)}>
+              Upload Document
+            </button>
+          </form>
+        </Modal>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1> Document Successfully Uploded</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
